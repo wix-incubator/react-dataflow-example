5a521145eb3a49be1bf2d0c1c529db31f7841c3d
talkol <talkol@wix.com>
2016-10-15 22:43:17 +0300

    display posts in PostsScreen

--- src/containers/PostsScreen.js
+++ src/containers/PostsScreen.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './PostsScreen.css';
 import * as postsActions from '../store/posts/actions';
+import * as postsSelectors from '../store/posts/reducer';
+import ListView from '../components/ListView';
+import ListRow from '../components/ListRow';
 
 class PostsScreen extends Component {
 
@@ -10,16 +13,43 @@ class PostsScreen extends Component {
   }
 
   render() {
+    if (!this.props.rowsById) return this.renderLoading();
     return (
-      <h2>Done</h2>
+      <div className="PostsScreen">
+        <ListView
+          rowsIdArray={this.props.rowsIdArray}
+          rowsById={this.props.rowsById}
+          renderRow={this.renderRow.bind(this)} />
+      </div>
     );
+  }
+
+  renderLoading() {
+    return (
+      <p>Loading...</p>
+    );
+  }
+
+  renderRow(rowId, row) {
+    return (
+      <ListRow rowId={rowId}>
+        {!row.thumbnail ? false :
+          <img className="thumbnail" src={row.thumbnail} />
+        }
+        <h3>{row.title}</h3>
+      </ListRow>
+    )
   }
 
 }
 
 // which props do we want to inject, given the global store state?
 function mapStateToProps(state) {
-  return {};
+  const [postsById, postsIdArray] = postsSelectors.getPosts(state);
+  return {
+    rowsById: postsById,
+    rowsIdArray: postsIdArray
+  };
 }
 
 export default connect(mapStateToProps)(PostsScreen);
--- src/store/posts/reducer.js
+++ src/store/posts/reducer.js
@@ -4,6 +4,8 @@
 // the other exports in this file are selectors, which is business logic that digests parts of the store's state
 // for easier consumption by views
 
+import _ from 'lodash';
+import * as types from './actionTypes';
 import Immutable from 'seamless-immutable';
 
 const initialState = Immutable({
@@ -14,7 +16,19 @@ const initialState = Immutable({
 
 export default function reduce(state = initialState, action = {}) {
   switch (action.type) {
+    case types.POSTS_FETCHED:
+      return state.merge({
+        postsById: action.postsById
+      });
     default:
       return state;
   }
+}
+
+// selectors
+
+export function getPosts(state) {
+  const postsById = state.posts.postsById;
+  const postsIdArray = _.keys(postsById);
+  return [postsById, postsIdArray];
 }
