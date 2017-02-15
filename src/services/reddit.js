2ca5357de35286a581934f591558d732aa6c005d
Daniel Zlotin <danielzl@wix.com>
2017-02-15 11:18:42 +0200

    extract till you drop

--- src/services/reddit.js
+++ src/services/reddit.js
@@ -12,12 +12,8 @@ const REDDIT_ENDPOINT = 'https://www.reddit.com';
 const DEFAULT_SUBREDDITS_ENDPOINT = `${REDDIT_ENDPOINT}/subreddits/default.json`;
 
 export async function getDefaultSubreddits() {
-  const data = await http.get(DEFAULT_SUBREDDITS_ENDPOINT);
-  const children = _.get(data, 'data.children');
-  if (!children) {
-    throw new Error(`RedditService getDefaultSubreddits failed, children not returned`);
-  }
-  return _.map(children, (subreddit) => {
+  const subreddits = await getAndValidateSubreddits();
+  return _.map(subreddits, (subreddit) => {
     // abstract away the specifics of the reddit API response and take only the fields we care about
     return {
       title: _.get(subreddit, 'data.display_name'),
@@ -26,6 +22,15 @@ export async function getDefaultSubreddits() {
       subscribers: _.get(subreddit, 'data.subscribers')
     }
   });
+}
+
+async function getAndValidateSubreddits() {
+  const data = await http.get(DEFAULT_SUBREDDITS_ENDPOINT);
+  const children = _.get(data, 'data.children');
+  if (!children) {
+    throw new Error(`RedditService getDefaultSubreddits failed, children not returned`);
+  }
+  return children;
 }
 
 
