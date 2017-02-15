d7f1d33747b0b7fa1714f799bc815daf91574e3d
Daniel Zlotin <danielzl@wix.com>
2017-02-15 12:35:47 +0200

    store holds posts by id

--- src/stores/posts/store.js
+++ src/stores/posts/store.js
@@ -1,5 +1,21 @@
+import _ from 'lodash';
+
+const state = {
+  postsById: {}
+};
+
+export const setters = {
+  setPosts(posts) {
+    state.postsById = _.keyBy(posts, (p) => p.id);
+  }
+};
+
 export const getters = {
   isLoading() {
     return true;
+  },
+
+  getPostsById() {
+    return state.postsById;
   }
-}
+};
--- src/stores/posts/store.test.js
+++ src/stores/posts/store.test.js
@@ -8,4 +8,12 @@ describe('posts store', () => {
   it('initial is loading', () => {
     expect(store.getters.isLoading()).toBe(true);
   });
+
+  it('holds posts by id', () => {
+    expect(store.getters.getPostsById()).toEqual({});
+    const post1 = { id: 'id1' };
+    const post2 = { id: 'id2' };
+    store.setters.setPosts([post1, post2]);
+    expect(store.getters.getPostsById()).toEqual({ id1: post1, id2: post2 });
+  });
 });
