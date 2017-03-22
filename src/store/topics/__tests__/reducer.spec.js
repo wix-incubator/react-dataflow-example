0e59fd0aee04468e2ba98ad63c7d74f88a7ea22e
Ethan Sharabi <ethans@wix.com>
2017-03-22 16:55:52 +0200

    test use case with real state chaing action

--- src/store/topics/__tests__/reducer.spec.js
+++ src/store/topics/__tests__/reducer.spec.js
@@ -1,5 +1,7 @@
+import Immutable from 'seamless-immutable';
 import { Reducer } from 'redux-testkit';
 import uut from '../reducer';
+import * as actionTypes from '../actionTypes';
 
 const initialState = {
   topicsByUrl: undefined,
@@ -16,4 +18,16 @@ describe('store/topics/reducer', () => {
     Reducer(uut).expect({type: 'NOT_EXISTING'}).toReturnState(initialState);
   });
   
+  it('should store fetched topics', () => {
+    const topicsByUrl = {url1: 'topic1', url2: 'topic2'};
+    const action = {type: actionTypes.TOPICS_FETCHED, topicsByUrl};
+    Reducer(uut).expect(action).toReturnState({...initialState, topicsByUrl});
+  });
+  
+  it('should store fetched topics and override existing topics', () => {
+    const exisingState = Immutable({...initialState, topicsByUrl: {url3: 'topic3'}});
+    const topicsByUrl = {url1: 'topic1', url2: 'topic2'};
+    const action = {type: actionTypes.TOPICS_FETCHED, topicsByUrl};
+    Reducer(uut, exisingState).expect(action).toReturnState({...initialState, topicsByUrl});
+  });
 });
